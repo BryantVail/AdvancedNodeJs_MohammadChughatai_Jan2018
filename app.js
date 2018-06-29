a865a86b77b172ccff1a5ebab676cf92cfe5959d
Bryant Vail <Bryanttv@outlook.com>
2018-06-29 04:02:50 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //app.js
+const express       = require("express");
+const app           = express();
 
+const mongodb       = require("mongodb");
+const mongoose      = require("mongoose");
+let   port          = process.env.PORT || 3000;
 
-
+app.listen(port, (error) =>{
+    if(!error){
+        console.log(
+            `listenting on port: ${port}`
+        );
+    }else{
+        console.log(error);
+    }
+});
 
 
 
