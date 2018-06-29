cde4a820e9107daa70174cf272dd458b010960da
Bryant Vail <Bryanttv@outlook.com>
2018-06-29 06:05:59 -0400

    users.model.js

--- Models/users.model.js
+++ Models/users.model.js
@@ -1,1 +1,28 @@
 //users.model.js
+const mongoose      = require("mongoose");
+const Schema        = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username:{
+        type        :String,
+        required    :true
+    },
+    password:{
+        type        :String,
+        required    :true
+    },
+    isOver21:{
+        type    :Boolean,
+        required    :true
+    }
+});
+
+
+
+
+
+
+//mongoose.model(modelName, schema)
+module.exports = mongoose.model("User",UserSchema);
+
+
