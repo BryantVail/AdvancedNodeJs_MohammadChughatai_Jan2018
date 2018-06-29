ac8964bd31354e034a88230e3e38d27d06783fea
Bryant Vail <Bryanttv@outlook.com>
2018-06-29 04:26:42 -0400

    user.model.js added mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //app.js
+"use strict";
 const express       = require("express");
 const app           = express();
 
 const mongodb       = require("mongodb");
 const mongoose      = require("mongoose");
+const keys          = require("./config/keys");
 let   port          = process.env.PORT || 3000;
 
+//models
+const User          = require("./Models/user.model.js");
+
+//app start
 app.listen(port, (error) =>{
     if(!error){
         console.log(
@@ -16,6 +22,16 @@ app.listen(port, (error) =>{
     }
 });
 
+//db connect
+mongoose.connect(keys.mlab.mongodbURI, (error) =>{
+    if(!error){
+        console.log(
+            "MongoDB connected"
+        );
+    }else{
+        console.log(error);
+    }
+});
 
 
 
